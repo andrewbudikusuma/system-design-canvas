2502592da3f2ca5ae6010bcfffb698b217828be3
Andrew Budikusuma <andrew.budikusuma@gmail.com>
2023-08-22 23:25:33 +0700

    (feat) write first test case

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,11 +1,2 @@
-const Hello = (name?: string) => `${name || 'World'}`;
-
-const user = {
-  jobTitle: 'Software Engineer',
-};
-
-if ((user.jobTitle = 'Junior Engineer')) {
-  user.jobTitle = 'Bug Introducer';
-}
-
-export default Hello;
+export * from './Button';
+export * from './IconButton';
